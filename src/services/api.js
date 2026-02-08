83a25ddc6860fb9292af5937578b244c6092e1f4
ParkPing <105095888+securityprotect@users.noreply.github.com>
2026-02-08 16:23:49 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -4,10 +4,4 @@ const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
 
-// TEMP token (jab tak login backend ready nahi)
-API.interceptors.request.use((config) => {
-  config.headers.Authorization = "Bearer testtoken";
-  return config;
-});
-
 export default API;
