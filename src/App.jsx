92ae8d0f9655180ad56a2384aea9647b89410399
ParkPing <105095888+securityprotect@users.noreply.github.com>
2026-02-08 16:44:35 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
-import { useState } from "react";
-import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
 
 function App() {
-  const [loggedIn, setLoggedIn] = useState(true); // TEMP AUTO LOGIN
-
-  return loggedIn ? <Dashboard /> : <Login onLogin={() => setLoggedIn(true)} />;
+  return <Dashboard />;
 }
 
 export default App;
